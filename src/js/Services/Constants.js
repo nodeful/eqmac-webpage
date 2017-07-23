c71f31725c1a57981780b8adfd4f0ea544273ad2
Romans Kisils <romankisilo@gmail.com>
2017-07-23 18:33:52 +0100

    some changes for release

--- src/js/Services/Constants.js
+++ src/js/Services/Constants.js
@@ -4,7 +4,7 @@
 			API_URL: 'https://eqmac-api.bitgapp.com',
 			STRIPE_PUBLIC_KEY: 'pk_live_yphzhbXG6DPiDG3KSlCP2iQA',
 			PAYPAL_PRODUCT_KEY: 'ATx9hGBwHY4KQJZ3SkMqf8aucE6qXmTDjzz_PgLAuK8e1mW1AmFc75fANl_lDmIOVJLE27bd8hAAEr1Z',
-			BINARY_URL: 'https://storage.googleapis.com/bitgapp/eqMac.dmg'
+			BINARY_URL: 'https://storage.googleapis.com/bitgapp/eqMac2.dmg'
 		})
 })();
 
